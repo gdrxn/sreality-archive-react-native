c9a730483fce8a4589c7041ef0baa5e2b43495cd
Anh Viet <tomicekn@gmail.com>
2022-10-17 20:45:13 +0200

    added infinity scrolling

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -8,30 +8,42 @@ const HomeScreen = () => {
 	const navigation = useNavigation();
 
 	const [products, setProducts] = useState([]);
+	const [page, setPage] = useState(1);
+	const [productsLength, setProductsLength] = useState(null);
 
-	useEffect(() => {
+	function getProducts() {
+		if (products.length === productsLength) return;
+
 		axios({
 			method: "get",
-			url: "http://127.0.0.1:3000/api/user/products",
+			url: `http://127.0.0.1:3000/api/user/products?page=${page}`,
 			withCredentials: true,
 		})
 			.then((res) => {
-				setProducts(res.data.currentProducts);
+				setProducts([...products, ...res.data.currentProducts]);
+				setProductsLength(res.data.productsLength);
+				setPage(page + 1);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
+	}
+
+	useEffect(() => {
+		getProducts();
 	}, []);
 
 	return (
 		<SafeAreaView className="bg-white flex-1">
 			<FlatList
+				onEndReachedThreshold={0.5}
+				onEndReached={getProducts}
 				data={products}
 				renderItem={({ item }) => (
 					<TouchableOpacity
 						className="py-8 border-b border-b-gray-100"
 						onPress={() => {
-							navigation.navigate("DetailsScreen", { productId: item._id });
+							navigation.navigate("Details", { productId: item._id });
 						}}
 					>
 						<Text className="text-center text-base">{item.name}</Text>
